9e1f5afc7ac0c99be6725a83eefa904109d354a1
blqnk3d <103069667+blqnk3d@users.noreply.github.com>
2024-06-12 00:02:00 +0200

    changes and stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,61 +1,36 @@
 extends CharacterBody2D
 
-# Speed of the player
-@export var speed: float = 150.0
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -200.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var sprite = $AnimatedSprite2D
-var inStair = false
 
-func _process(_delta: float) -> void:
+func _physics_process(delta):
+	sprite.play("idle")
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
 	
-		 
-	if  Input.is_action_pressed("interact") and inStair == true:
-		print("collided and pressed")
-	
-		inStair = false
-		
-	
-	# Create a new Vector2 to store the movement direction
-	var direction = Vector2.ZERO
-	
-	# Check for input and set the direction accordingly
-	if Input.is_action_pressed("up"):
-		direction.y -= 1
-	if Input.is_action_pressed("down"):
-		direction.y += 1
-	if Input.is_action_pressed("left"):
-		direction.x -= 1
+	if direction == -1:
 		sprite.flip_h = true
-	if Input.is_action_pressed("right"):
-		direction.x += 1
+	elif direction == 1:
 		sprite.flip_h = false
-
-	# Normalize direction to prevent faster diagonal movement and scale by speed
-	if direction != Vector2.ZERO:
-		direction = direction.normalized() * speed
-	var directionStandinOrRunning = Input.get_axis("left","right")
 	
-	if directionStandinOrRunning == 0:
-		sprite.play("idle")
+	if direction:
+		velocity.x = direction * SPEED
 	else:
-		sprite.play("walk")
-	
-	# Move the character using move_and_slide
-	velocity = direction
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	move_and_slide()
-	
-
-
-
-
-
-
-func _on_area_2d_area_entered(area):
-	if area.is_in_group("stair"):
-		inStair = true
-		
-
-
-func _on_area_2d_area_exited(area):
-	if area.is_in_group("stair"):
-		inStair = false
