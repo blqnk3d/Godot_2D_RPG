b722f9e3a9d1d7b13254217a2fa6f14bd5f89b18
julian Sieberer <julian.sieberer@htlstp.at>
2024-06-12 14:32:37 +0200

    added new blocks

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,12 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -200.0
+const JUMP_VELOCITY = -250.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var sprite = $AnimatedSprite2D
+@onready var player = $"."
+
 
 func _physics_process(delta):
 	sprite.play("idle")
@@ -34,3 +36,18 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func setPositionRel(xOff,yOff):
+	print("x: ",player.global_transform.origin.x,"y: ",player.global_transform.origin.y)
+	var position = Vector2(player.global_transform.origin.x+ xOff,player.global_transform.origin.y + yOff)
+	player.global_transform.origin = position
+
+func setPosition(x,y):
+	var position = Vector2(x,y)
+	player.global_transform.origin = position
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("death"):
+		setPosition(0,0)
+	elif area.is_in_group("spring"):
+		player.set_velocity(Vector2(player.global_transform.origin.x,-450))
