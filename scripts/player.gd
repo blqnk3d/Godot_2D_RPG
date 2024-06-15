0a95176c2034c17bd6f67db7f3f77f936e4f9b88
blqnk3d <103069667+blqnk3d@users.noreply.github.com>
2024-06-15 22:17:07 +0200

    Changed the Style to more Pokemon

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,36 +16,35 @@ var attack = 5
 var coins = 0
 
 
-
 @onready var sprite = $AnimatedSprite2D
 @onready var player = $"."
 @onready var hpBar = $ProgressBar
 @onready var showDebug = $Label
 @onready var chest = $"../chest"
 
+
 func _physics_process(delta):
 	hpBar.value = health
-	showDebug.text = "coins: "+ str(coins)
 	sprite.play("idle")
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	if Input.is_action_just_pressed("interact") :
-		chest.showInventory()
-		print("interacted")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	
+	
 	if direction == -1:
 		sprite.flip_h = true
 	elif direction == 1:
 		sprite.flip_h = false
+		
+	if Input.is_action_pressed("up"):
+		velocity.y = -1 * SPEED
+	elif Input.is_action_pressed("down"):
+		velocity.y = 1 * SPEED
+	else :
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
 	if direction:
 		velocity.x = direction * SPEED
@@ -54,22 +53,6 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-func setPositionRel(xOff,yOff):
-	print("x: ",player.global_transform.origin.x,"y: ",player.global_transform.origin.y)
-	var position = Vector2(player.global_transform.origin.x+ xOff,player.global_transform.origin.y + yOff)
-	player.global_transform.origin = position
 
-func setPosition(x,y):
-	var position = Vector2(x,y)
-	player.global_transform.origin = position
 
-func _on_area_2d_area_entered(area):
-	if area.is_in_group("death"):
-		print("fell into the void")
-		setPosition(0,0)
-	elif area.is_in_group("spring"):
-		print("spring detectet")
-		player.set_velocity(Vector2(player.global_transform.origin.x,-450))
-	elif  area.is_in_group("coin"):
-		print("coin detected")
-		coins += 1
+
