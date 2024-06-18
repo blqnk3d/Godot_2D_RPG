185d7db678328ee279a975bf4027159a2890348d
blqnk3d <103069667+blqnk3d@users.noreply.github.com>
2024-06-18 19:15:38 +0200

    Added pick up stuff ny folders and a coind that is stackable

--- InventoryPlayer.gd
+++ InventoryPlayer.gd
@@ -3,3 +3,17 @@ extends Resource
 class_name PlayerInv
 
 @export var items : Array[InvItem]
+
+
+
+
+func add (item : InvItem):
+	print(item in items)
+	if item in items:
+		var index = items.find(item )
+		if index != -1:
+			items[index].amount += 1
+			return
+	else:
+		items.append(item)
+	print(items)
---
+++ scripts/coin.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var ownItem : InvItem
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+
+func _on_pick_up_component_area_entered(area ):
+	if area is HitboxComponent:
+		if area.is_in_group("player"):
+			var player : Player = area.get_parent()
+			player.invPlayer.add(ownItem)
+			queue_free()
